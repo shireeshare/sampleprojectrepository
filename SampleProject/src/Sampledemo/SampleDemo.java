43d975dac6f2508c28c37ad4b4d340496b879bc9
hai <hai@DESKTOP-HVD9T44>
2022-08-03 11:49:19 +0530

    sample project is committed successfully

---
+++ SampleProject/src/Sampledemo/SampleDemo.java
@@ -0,0 +1,11 @@
+package Sampledemo;
+
+public class SampleDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("sample project is successfully cpmmited");
+
+	}
+
+}
